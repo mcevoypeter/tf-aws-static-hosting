c7e6be0f3b6591d316f4ba6043f8ce52d34d053e
Peter McEvoy <git@mcevoypeter.com>
2023-10-31 17:38:07 -0400

    Add public access block for S3 bucket

--- main.tf
+++ main.tf
@@ -10,3 +10,11 @@ terraform {
 resource "aws_s3_bucket" "this" {
   bucket = var.domain
 }
+
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket                  = aws_s3_bucket.this.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
