9f08dc4b4b7940efa596822fdb6cb0dff313d67f
Peter McEvoy <git@mcevoypeter.com>
2023-11-01 12:05:30 -0400

    Add CloudFront distribution with access to S3 bucket

--- main.tf
+++ main.tf
@@ -18,3 +18,62 @@ resource "aws_s3_bucket_public_access_block" "this" {
   ignore_public_acls      = false
   restrict_public_buckets = false
 }
+
+data "aws_iam_policy_document" "allow_access_from_cloudfront" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.this.arn}/*"]
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = [aws_cloudfront_distribution.this.arn]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "this" {
+  bucket = aws_s3_bucket.this.id
+  policy = data.aws_iam_policy_document.allow_access_from_cloudfront.json
+}
+
+resource "aws_cloudfront_distribution" "this" {
+  enabled             = true
+  default_root_object = "index.html"
+  # see https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/private-content-restricting-access-to-s3.html#create-oac-overview-s3
+  origin {
+    domain_name              = aws_s3_bucket.this.bucket_regional_domain_name
+    origin_id                = aws_s3_bucket.this.id
+    origin_access_control_id = aws_cloudfront_origin_access_control.this.id
+  }
+  # AWS Managed-CachingOptimized
+  default_cache_behavior {
+    cache_policy_id  = data.aws_cloudfront_cache_policy.managed_caching_optimized.id
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = aws_s3_bucket.this.id
+    # Redirect HTTP to HTTPS.
+    viewer_protocol_policy = "redirect-to-https"
+  }
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+      locations        = []
+    }
+  }
+}
+
+resource "aws_cloudfront_origin_access_control" "this" {
+  name                              = aws_s3_bucket.this.bucket_regional_domain_name
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+data "aws_cloudfront_cache_policy" "managed_caching_optimized" {
+  name = "Managed-CachingOptimized"
+}
