3860fe7e5e309025ab2c5eafa7cf8897fa043c22
Peter McEvoy <git@mcevoypeter.com>
2023-11-02 14:33:57 -0400

    Add AWS region variable

--- main.tf
+++ main.tf
@@ -8,6 +8,10 @@ terraform {
 }
 
 provider "aws" {
+  region = var.region
+}
+
+provider "aws" {
   alias  = "us_east_1"
   region = "us-east-1"
 }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,9 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
 variable "domain" {
   description = "Domain to host at"
   type        = string
