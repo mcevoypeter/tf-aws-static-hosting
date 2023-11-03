64cd58e2eb16c3918df6033b250689f46ac01896
Peter McEvoy <git@mcevoypeter.com>
2023-11-03 14:30:14 -0400

    Remove AWS provider
    Providers should not be defined within modules per
    https://developer.hashicorp.com/terraform/language/modules/develop/providers.

--- variables.tf
+++ variables.tf
@@ -1,9 +1,3 @@
-variable "region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-east-1"
-}
-
 variable "domain" {
   description = "Domain to host at"
   type        = string
